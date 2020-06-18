5fbc4240cd57ca07e7542bdd66cb0c83298d0cb3
Hosein Dadkhah <dadkhah.ir@gmail.com>
2020-06-18 16:22:39 +0430

    fix datetime format and stop extra outputs

--- src/ArticleScrapping.py
+++ src/ArticleScrapping.py
@@ -7,12 +7,16 @@ from newspaper import Article
 from datetime import datetime
 import lxml, lxml.html
 
+sys.stdout = open(os.devnull, "w") #To prevent a function from printing in the batch console in Python
+
 url = functionName = sys.argv[1]
 article = Article(url)
 article.download() #Downloads the link’s HTML content
 article.parse() #Parse the article
 nltk.download('all')#1 time download of the sentence tokenizer
 article.nlp()#  Keyword extraction wrapper
+
+sys.stdout = sys.__stdout__
 
 data = article.__dict__
 del data['config']
@@ -22,7 +26,7 @@ for i in data:
     if type(data[i]) is set:
         data[i] = list(data[i])
     if type(data[i]) is datetime:
-        data[i] = data[i].strftime("%Y-%m-%d %H:%i:%s")
+        data[i] = data[i].strftime("%Y-%m-%d %H:%M:%S")
     if type(data[i]) is lxml.html.HtmlElement:
         data[i] = lxml.html.tostring(data[i])
     if type(data[i]) is bytes:
